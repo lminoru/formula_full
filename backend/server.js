1dd9f4d1dded67d5f95a6250789c7072ca4c2bf1
lminoru <leo.minoru@hotmail.com>
2023-07-12 18:44:30 -0300

    remove comment

--- backend/server.js
+++ backend/server.js
@@ -314,47 +314,6 @@ app.get('/piloto_resultados_estado', async (req, res) => {
     }
 })
 
-// // Exemplo de rota para obter dados do banco de dados
-// app.get('/dados', async (req, res) => {
-//     try {
-//         const query = 'SELECT * FROM Users WHERE';
-//         const result = await pool.query(query);
-//         res.json(result.rows);
-//     } catch (error) {
-//         console.error('Erro ao obter dados do banco de dados', error);
-//         res.status(500).json({ error: 'Erro ao obter dados do banco de dados' });
-//     }
-// });
-
-// // Rota para obter um registro específico
-// app.get('/dados/:id', async (req, res) => {
-//     const { id } = req.params;
-//     try {
-//       const query = 'SELECT * FROM tabela WHERE id = $1';
-//       const result = await pool.query(query, [id]);
-//       if (result.rows.length === 0) {
-//         return res.status(404).json({ error: 'Registro não encontrado' });
-//       }
-//       res.json(result.rows[0]);
-//     } catch (error) {
-//       console.error('Erro ao obter o registro do banco de dados', error);
-//       res.status(500).json({ error: 'Erro ao obter o registro do banco de dados' });
-//     }
-//   });
-
-//   // Rota para criar um novo registro
-//   app.post('/dados', async (req, res) => {
-//     const { nome, idade } = req.body;
-//     try {
-//       const query = 'INSERT INTO tabela (nome, idade) VALUES ($1, $2) RETURNING *';
-//       const result = await pool.query(query, [nome, idade]);
-//       res.json(result.rows[0]);
-//     } catch (error) {
-//       console.error('Erro ao criar o registro no banco de dados', error);
-//       res.status(500).json({ error: 'Erro ao criar o registro no banco de dados' });
-//     }
-//   });
-
 // Inicialização do servidor
 app.listen(8090, () => {
     console.log('Servidor está executando na porta 8090');
